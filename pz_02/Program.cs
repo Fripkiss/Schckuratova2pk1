816a75dce6f8a48410a3e1497885bc29b466f256
Мария Шкуратова <schckuratova.mar@yandex.ru>
2023-09-17 13:32:36 +0500

    добавила третью практику

---
+++ pz_02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace pz_02;
+
+  internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите переменную a")
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Введите переменную b);
+        double b = Convert.ToDouble(Console.ReadLine());
+        double 
+    }
+        
+}
---
+++ pz_03/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace pz_03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите скорость ветра: ");
+        int speed = Convert.ToInt32(Console.ReadLine());
+        int value = speed >= 1 && speed <= 4 ? 1 : speed >= 5 && speed <= 10 ? 2 :
+            speed >= 11 && speed <= 18 ? 3 : 4;
+        switch (value)
+        {
+            case 1:
+                Console.WriteLine("Слабый ветер.");
+                break;
+            case 2:
+                Console.WriteLine("Умеренный ветер.");
+                break;
+            case 3:
+                Console.WriteLine("Сильный ветер.");
+                break;
+            case 4:
+                Console.WriteLine("Ураган.");
+                break;
+
+        }
+}
