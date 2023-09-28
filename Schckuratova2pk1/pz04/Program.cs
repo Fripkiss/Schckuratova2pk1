1fb193941b273cecf131a9b6468c9fdb68ee101c
Мария Шкуратова <schckuratova.mar@yandex.ru>
2023-09-28 20:06:48 +0500

    добавила 6 практику

---
+++ Schckuratova2pk1/pz04/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace pz04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 30; i<=150; i += 15)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ pz_06/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace pz_06;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[] arr = new char[10];
+
+        Console.WriteLine("Введите 10 символов для заполнения массива: ");
+        for (int i = 0; i < 10; i++)
+        {
+            arr[i] = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        char maxElement = GetMaxElement(arr);
+        Console.WriteLine("Максимальный элемент массива - " + maxElement);
+
+        Array.Sort(arr);
+        Console.WriteLine("Элементы массива в порядке возрастания: ");
+        foreach (char element in arr)
+        {
+            Console.WriteLine(element);
+        }
+
+        Console.ReadLine();
+    }
+
+    static char GetMaxElement(char[] arr)
+    {
+        char max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
