0db14bb3250ff4bdd265ff7bece8e0a8114cd889
Мария Шкуратова <schckuratova.mar@yandex.ru>
2023-11-18 12:03:17 +0500

    Добавила 9 практику

--- pz_08/Program.cs
+++ pz_08/Program.cs
@@ -9,23 +9,23 @@ namespace pz_08
             Console.WriteLine("Задание 1 и 2");
             Console.WriteLine();
             Random random = new Random();
-            double[][] arr = new double[10][]; // создаем двумерный массив вещественных чисел размером 10
+            int[][] arr = new int[10][]; // создаем двумерный массив целых чисел размером 10
             for (int i = 0; i < arr.Length; i++) // генерируем массив
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    arr[j] = new double[random.Next(2, 31)]; // задаем случайный размер для каждого элемента массива
+                    arr[j] = new int[random.Next(2, 31)]; // задаем случайный размер для каждого элемента массива
                 }
             }
 
             Console.WriteLine();
-            double rndNum;
+            int rndNum;
 
             for (int i = 0; i < arr.Length; i++)
             {
                 for (int j = 0; j < arr[i].Length; j++)
                 {
-                    rndNum = random.NextDouble() * (100 - 0) + 0; // генерируем случайное вещественное число от 0 до 100
+                    rndNum = random.Next(0, 101); // генерируем случайное целое число от 0 до 100
                     arr[i][j] = rndNum;
                     Console.Write(arr[i][j] + " ");
                 }
@@ -34,8 +34,7 @@ namespace pz_08
             Console.WriteLine("Задание 3");
             Console.WriteLine();
 
-            double[] lastNum = new double[10]; // создаем массив для хранения последнего числа каждого элемента массива arr
-
+            int[] lastNum = new int[10]; // создаем массив для хранения последнего числа каждого элемента массива arr
 
             for (int i = 0; i < lastNum.Length; i++)
             {
@@ -57,8 +56,8 @@ namespace pz_08
             Console.WriteLine();
             for (int i = 0; i < arr.Length; i++)
             {
-                double b = arr[i][0]; // сохраняем первое число каждого элемента массива arr в переменную b
-                double c = arr[i].Max(); // находим максимальное значение в каждом элементе массива arr и сохраняем его в переменную c
+                int b = arr[i][0]; // сохраняем первое число каждого элемента массива arr в переменную b
+                int c = arr[i].Max(); // находим максимальное значение в каждом элементе массива arr и сохраняем его в переменную c
                 int n = Array.IndexOf(arr[i], c); // находим индекс максимального значения в каждом элементе массива arr и сохраняем его в переменную n
                 arr[i][0] = c; // присваиваем первому элементу каждого элемента массива arr значение c
                 arr[i][n] = b; // присваиваем элементу массива arr по индексу n значение b
@@ -81,7 +80,6 @@ namespace pz_08
                 Array.Reverse(arr[i]);
             }
 
-
             for (int i = 0; i < arr.Length; i++) // Вывод массива после реверса
             {
                 for (int j = 0; j < arr[i].Length; j++)
@@ -97,14 +95,17 @@ namespace pz_08
 
             for (int i = 0; i < arr.Length; i++)
             {
-                double sum = 0;
+                int sum = 0;
                 for (int j = 0; j < arr[i].Length; j++)
                 {
                     sum += arr[i][j];
                 }
-                double average = sum / arr[i].Length;
+                double average = (double)sum / arr[i].Length;
                 Console.WriteLine($"Среднее значение в строке {i + 1}: {average}");
             }
+
+
+
 
 
         }
---
+++ pz_09/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace pz_09;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+                break;
+
+            bool isOnlyLetters = true;
+            foreach (char c in input)
+            {
+                if (!char.IsLetter(c))
+                {
+                    isOnlyLetters = false;
+                    break;
+                }
+            }
+
+            if (isOnlyLetters)
+            {
+                Console.WriteLine(input);
+                break;
+            }
+        }
+    }
+}
