ecc3382d9454e7574e67960473f2163745e7760e
Мария Шкуратова <schckuratova.mar@yandex.ru>
2023-11-24 18:34:14 +0500

    добавила 10 практику

---
+++ pz_10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pz_10;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        string updatedText = ""; 
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '(')
+            {
+                insideBrackets = true; 
+            else if (text[i] == ')')
+                {
+                    insideBrackets = false; 
+            else if (!insideBrackets)
+                    {
+                        updatedText += text[i]; 
+                    }
+                    Console.WriteLine(updatedText);
+                }
+            }
