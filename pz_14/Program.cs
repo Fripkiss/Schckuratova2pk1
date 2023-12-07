dd5051d143d1c8a240dc4b3fdff5fab4a96340b9
Мария Шкуратова <schckuratova.mar@yandex.ru>
2023-12-07 20:31:34 +0500

    добавила 14 практику

---
+++ pz_14/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace pz_14;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите путь к первому файлу:");
+        string filePath1 = Console.ReadLine();
+        Console.WriteLine("Введите путь ко второму файлу:");
+        string filePath2 = Console.ReadLine();
+        Console.WriteLine("Введите путь к файлу для записи результата:");
+        string outputFilePath = Console.ReadLine();
+
+        MergeFiles(filePath1, filePath2, outputFilePath);
+
+        Console.WriteLine("Слияние выполнено успешно!");
+    }
+
+    static void MergeFiles(string filePath1, string filePath2, string outputFilePath)
+    {
+        string[] lines1 = File.ReadAllLines(filePath1);
+        string[] lines2 = File.ReadAllLines(filePath2);
+
+        using (StreamWriter sw = new StreamWriter(outputFilePath))
+        {
+            for (int i = 0; i < Math.Max(lines1.Length, lines2.Length); i++)
+            {
+                if (i < lines1.Length)
+                {
+                    sw.WriteLine(lines1[i]);
+                }
+                if (i < lines2.Length)
+                {
+                    sw.WriteLine(lines2[i]);
+                }
+            }
+        }
+    }
+}
